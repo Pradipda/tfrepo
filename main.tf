c886d7d8c280c031ee27ee0c984cc022ceada470
Pradip <mr.pratheeban@gmail.com>
2025-09-24 12:46:48 +0530

    my 3rd commit after gui variables

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "Pradip13VPC" {
-  cidr_block = "10.13.0.0/16"
+  cidr_block = "10.131.0.0/16"
   tags = {
-    name = "pradip-gitvpc"
+    name = "pradip-gitvpc-v2"
     env = "env from local sys"
   }
 }
